7c11e3180c018c315b8906d6bb0e2d022971280d
Victor Tennekes <victortennekes@icloud.com>
2025-09-22 14:25:13 +0200

    feat: add split keybinds nvim

--- nvim/lua/config/personal/remap.lua
+++ nvim/lua/config/personal/remap.lua
@@ -1,1 +1,13 @@
 vim.g.mapleader = " "
+
+-- Set keybinds for split screen navigation (Ctrl + hjkl)
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+-- Leader keybindings for splits
+vim.keymap.set('n', '<leader>vs', ':vsplit<CR>', opts)
+vim.keymap.set('n', '<leader>hs', ':split<CR>', opts)
